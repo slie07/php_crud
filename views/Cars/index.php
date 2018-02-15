0f689ce86ba28f8a830225d4062c94aecc8d11a4
slie07 <liestevano@gmail.com>
2018-02-14 19:12:38 -0700

    added folder

--- views/Cars/index.php
+++ views/Cars/index.php
@@ -3,11 +3,12 @@
 <head>
 	<body>
 		<h1>Add a New Car To The List</h1>
-        <form action="create" method="POST">
+        <!-- <form action="create" method="POST">
             <input type="text" name="car" placeholder="Geo- Metro"/><br/>
             <input type="text" name="owner" placeholder="Name Please ?"/><br/>
             <input type="submit" value="Submit"/>
-        </form>
+        </form> -->
+        <a href="new" > add new car  </a>
 	<title>Index cars</title>
 	  <section>
       <ul>
---
+++ views/Cars/new.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<h1>Add a New Car To The List</h1>
+        <form action="create" method="POST">
+            <input type="text" name="car" placeholder="Geo- Metro"/><br/>
+            <input type="text" name="owner" placeholder="Name Please ?"/><br/>
+            <input type="submit" value="Submit"/>
+        </form>
+
+</body>
+</html>
